cb3c0a50de24af8ff9addcffb677a8bd01d2af4e
UMSC_Official <cao_junkai@qq.com>
2024-07-04 19:11:36 +0800

    Update

--- functions.py
+++ functions.py
@@ -1,9 +1,25 @@
-import os, time
-def comp(f, d): os.system(f"7z a {f} \"{d}\\**\"") # 压缩
-def extr(f, d): os.system(f"7z x {f} -o{d} -aoa") # 解压
+import os, time, json
+def comp(f, d): os.system(f"7z a {f} \"{d}\\**\"") # Compress
+def extr(f, d): os.system(f"7z x {f} -o{d} -aoa") # Extract
 def getOjng(v): return f"{os.environ.get("LocalAppData")}\\Packages\\Microsoft.Minecraft{["UWP", "WindowsBeta"][v]}_8wekyb3d8bbwe\\LocalState\\games\\com.mojang"
 def getTime(d): return time.strftime("%Y%m%d%H%M%S")[0:d]
+def temSize(i): return [os.get_terminal_size().columns, os.get_terminal_size().lines][i]
+def cls(): os.system('cls')
+def folder(p): os.system(f'explorer {p}')
+def readJSON(f): # Read & return JSON file data
+	with open(f, "r", encoding="UTF-8") as file:
+		json_str = file.read()
+		return json.loads(json_str)
+def writeJSON(f, data): # Write data into JSON file
+	with open(f, "w", encoding="UTF-8") as file:
+		json.dump(data, file, ensure_ascii=False, indent='\t')
+
 # comp("1.zip", getOjng(0))
 # extr("1.zip", ".\\t")
 # print(f"Release: {getOjng(0)}\nPreview: {getOjng(1)}")
 # print(getTime(12))
+# print(f'Width:  {temSize(0)}\nHeight: {temSize(1)}')
+# cls()
+# jsonData = readJSON("example_config.json")
+# print(jsonData)
+# writeJSON("test.json", jsonData)
--- main.py
+++ main.py
@@ -1,3 +1,51 @@
-import os
-from functions import comp, extr, getOjng, getTime
-from json_rw import readJSON, writeJSON
+import os, time
+from functions import comp, getTime, temSize, cls, folder, readJSON
+
+cfg = readJSON('config.json')
+def main():
+	cls()
+	print(f'文件夹备份工具\n{'=' * temSize(0)}')
+	for i in cfg:
+		print(f'''{i}: {cfg[i]['info']}
+    备份路径：{cfg[i]['bdir']}\n''')
+	print(f'0: 退出程序 (Ctrl+C)\n{'=' * temSize(0)}')
+	sel = input('输入选项编号后回车确定：')
+	if sel == '0': exit()
+	elif sel in cfg:
+		options(sel)
+	main()
+
+def options(i):
+	cls()
+	print(f'''{cfg[i]['info']}
+{'=' * temSize(0)}
+原始路径：{cfg[i]['path']}
+备份预览：{cfg[i]['bdir']}\{cfg[i]['head']}_{getTime(12)}.{cfg[i]['form']}
+{'=' * temSize(0)}
+1：开始备份
+2：打开原目录
+0：返回主界面
+{'=' * temSize(0)}''')
+	sel = input('输入选项编号后回车确定：')
+	if sel == '0': main()
+	elif sel == '1':
+		if not os.path.exists(cfg[i]['bdir']): os.makedirs(cfg[i]['bdir'])
+		comp(f'{cfg[i]['bdir']}\{cfg[i]['head']}_{getTime(12)}.{cfg[i]['form']}', cfg[i]['path'])
+		time.sleep(1)
+		ifolder(i)
+	elif sel == '2':
+		folder(cfg[i]['path'])
+		options(i)
+
+def ifolder(i):
+	cls()
+	print(f'''{cfg[i]['info']}：备份完成
+{'=' * temSize(0)}
+1：打开备份所在文件夹
+0：返回主界面
+{'=' * temSize(0)}''')
+	sel = input('输入选项编号后回车确定：')
+	if sel == '0': main()
+	elif sel == '1': folder(cfg[i]['bdir'])
+	main()
+main()
